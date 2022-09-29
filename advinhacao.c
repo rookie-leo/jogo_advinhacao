f4a4102a1ad6352bfbce4834ad48d5a30940563d
rookie-leo <proleonarpa@gmail.com>
2022-09-29 08:47:04 -0300

    Deixando o código legivel

--- advinhacao.c
+++ advinhacao.c
@@ -1,17 +1,52 @@
 #include <stdio.h>
 #include <locale.h>
 
-void main() {
+#define NUM_TENTATIVAS 5
+
+void main()
+{
     setlocale(LC_ALL, "Portuguese");
 
     printf("*****************************************\n");
     printf("*          Jogo da Advinhacao!          *\n");
     printf("*****************************************\n");
 
-    int num_secreto = 45, chute;
+    int num_secreto = 45, chute, acertou, maior;
 
-    printf("Digite um numero inteiro: ");
-    scanf("%d", &chute);
+    printf("Tente advinhar em qual numero estou pensando.\n");
 
-    printf("Seu chute foi %d", chute);
+    for (int i = 0; i < NUM_TENTATIVAS; i++)
+    {
+        printf("Tentativas: %d\n\n", NUM_TENTATIVAS - i);
+        
+        printf("Digite um numero inteiro: ");
+        scanf("%d", &chute);
+
+        printf("Seu chute foi %d\n", chute);
+
+        acertou = chute == num_secreto;
+
+        if (acertou)
+        {
+            printf("Parabens! Voce acertou!\n");
+            break;
+        }
+        else
+        {
+            printf("Voce errou!\n");
+
+            maior = chute > num_secreto;
+
+            if (maior)
+            {
+                printf("Seu chute foi maior que o numero que eu pensei!\n");
+            }
+            else
+            {
+                printf("Seu chute foi menor que o numero que eu pensei!\n");
+            }
+        }
+    }
+
+    printf("Jogue novamente!\n");
 }
